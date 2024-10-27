4855dfaeb97f3848b47d54b4e5f1972de4cb9b06
JYP <bluebinarycode@gmail.com>
2024-10-28 00:50:46 +0900

    Initial commit

---
+++ src/main/java/com/zero/bwtableback/BwTableBackApplication.java
@@ -0,0 +1,13 @@
+package com.zero.bwtableback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwTableBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwTableBackApplication.class, args);
+	}
+
+}
