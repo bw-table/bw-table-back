f50ff0fe2534e68030cd38fd3d1078c194b61f73
JYP <bluebinarycode@gmail.com>
2024-10-29 16:39:23 +0900

    feat: [ISSUE-007] Pagination 및 API 응답 클래스 구현

--- src/main/java/com/zero/bwtableback/member/service/MemberServiceImpl.java
+++ src/main/java/com/zero/bwtableback/member/service/MemberServiceImpl.java
@@ -22,10 +22,17 @@ public class MemberServiceImpl implements MemberService {
     @Override
     @Transactional
     public Member registerMember(SignupForm form) {
-        // 이메일 중복 체크
+        // 이메일 유효성 검사 및 중복 체크
+        validateEmail(form.getEmail());
         if (isEmailDuplicate(form.getEmail())) {
             throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
         }
+
+        // 닉네임 유효성 검사
+        validateNickname(form.getNickname());
+
+        // 비밀번호 유효성 검사
+        validatePassword(form.getPassword());
 
         // 비밀번호 암호화
         String encodedPassword = passwordEncoder.encode(form.getPassword());
@@ -39,5 +46,32 @@ public class MemberServiceImpl implements MemberService {
     // 이메일 중복 체크 함수
     public boolean isEmailDuplicate(String email) {
         return memberRepository.existsByEmail(email);
+    }
+    // 닉네임 중복 체크 함수
+    public boolean isNicknameDuplicate(String nickname) {
+        return memberRepository.existsByNickname(nickname);
+    }
+
+    // 이메일 유효성 검사
+    private void validateEmail(String email) {
+        if (email.length() < 3 || email.length() > 50 || !email.matches("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$")) {
+            throw new IllegalArgumentException("유효하지 않은 이메일 형식입니다.");
+        }
+    }
+    // 닉네임 유효성 검사
+    private void validateNickname(String nickname) {
+        if (nickname.length() < 3 || nickname.length() > 20 || !nickname.matches("^[a-zA-Z0-9]+$")) {
+            throw new IllegalArgumentException("유효하지 않은 닉네임입니다.");
+        }
+
+        if (isNicknameDuplicate(nickname)) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+    }
+    // 비밀번호 유효성 검사
+    private void validatePassword(String password) {
+        if (password.length() < 8 || !password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")) {
+            throw new IllegalArgumentException("비밀번호는 최소 8자 이상이며 대문자, 소문자, 숫자 및 특수문자를 포함해야 합니다.");
+        }
     }
 }
