ca275d72fc30c1b889111d16f681c7316cb86e8a
jina <jinahoi@naver.com>
2024-10-30 18:54:20 +0900

    feat: 예약 엔티티 생성 (#9)
    - 예약 일자, 예약 일시, 인원 수, 요청 사항, 예약 상태와 관련된 필드를 포함합니다.
    - ERD를 기반으로 가게, 회원, 결제, 알림과 관계 설정을 추가했고, 아직 관련 엔티티가 만들어지지 않아 주석으로 처리해 두었습니다.
    - 예약 상태 필드를 위해 Enum 클래스를 추가했습니다.

---
+++ src/main/java/com/zero/bwtableback/reservation/entity/ReservationStatus.java
@@ -0,0 +1,9 @@
+package com.zero.bwtableback.reservation.entity;
+
+public enum ReservationStatus {
+    WAITING, // 예약 대기
+    CONFIRMED, // 예약 확정
+    CANCELED,  // 예약 취소
+    VISITED // 방문 완료
+
+}
