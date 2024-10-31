eee95d16007194b9043e44d3945dbe150b999795
jina <jinahoi@naver.com>
2024-10-31 16:06:16 +0900

    chore: 레포지토리 메소드에 주석 추가
    - 코드 리뷰에 따라 각 쿼리 메서드에 대한 설명을 주석으로 추가했습니다.

--- src/main/java/com/zero/bwtableback/reservation/repository/ReservationRepository.java
+++ src/main/java/com/zero/bwtableback/reservation/repository/ReservationRepository.java
@@ -9,10 +9,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    //    Page<Reservation> findByRestaurantName(String restaurantName, Pageable pageable);
+    // 가게명으로 예약 목록을 조회
+    // Page<Reservation> findByRestaurantName(String restaurantName, Pageable pageable);
 
+    // 예약일자, 예약시간으로 예약 목록 조회
     Page<Reservation> findByReservationDateAndReservationTime(LocalDate date, LocalTime time, Pageable pageable);
 
-    //    Page<Reservation> findByRestaurantNameAndDate(String restaurantName, LocalDate date, Pageable pageable);
+    // 가게명, 예약일자로 예약 목록 조회
+    // Page<Reservation> findByRestaurantNameAndDate(String restaurantName, LocalDate date, Pageable pageable);
 
 }
