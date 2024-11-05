9810c5fd694f27c218816ad4c090660170ceb5c2
yeonseungji <tmddusahs@gmail.com>
2024-11-05 16:05:41 +0900

    feat: add review image entity and modify methods (#37)

--- src/main/java/com/zero/bwtableback/restaurant/service/ReviewService.java
+++ src/main/java/com/zero/bwtableback/restaurant/service/ReviewService.java
@@ -2,10 +2,12 @@ package com.zero.bwtableback.restaurant.service;
 
 import com.zero.bwtableback.restaurant.dto.ReviewInfoDto;
 import com.zero.bwtableback.restaurant.entity.Restaurant;
+import com.zero.bwtableback.restaurant.entity.ReviewImage;
 import com.zero.bwtableback.restaurant.repository.RestaurantRepository;
 import com.zero.bwtableback.restaurant.dto.ReviewReqDto;
 import com.zero.bwtableback.restaurant.dto.ReviewResDto;
 import com.zero.bwtableback.restaurant.entity.Review;
+import com.zero.bwtableback.restaurant.repository.ReviewImageRepository;
 import com.zero.bwtableback.restaurant.repository.ReviewRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,10 +26,10 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepository;
     private final RestaurantRepository restaurantRepository;
+    private final ReviewImageRepository reviewImageRepository;
 
     // 리뷰 작성
     public ReviewResDto createReview(Long restaurantId, ReviewReqDto reqDto) {
-
         Restaurant restaurant = restaurantRepository.findById(restaurantId)
                 .orElseThrow(() -> new EntityNotFoundException("Restaurant not found with id: " + restaurantId));
 
@@ -34,14 +38,24 @@ public class ReviewService {
                 .rating(reqDto.getRating())
                 .restaurant(restaurant)
                 .build();
-
         Review savedReview = reviewRepository.save(review);
 
-        return ReviewResDto.builder()
-                .id(savedReview.getId())
-                .restaurantId(savedReview.getRestaurant().getId())
-                .message("Review and rating added successfully")
-                .build();
+        Set<ReviewImage> images = new HashSet<>();
+        if (reqDto.getImages() != null && !reqDto.getImages().isEmpty()) {
+            for (String imageUrl: reqDto.getImages()) {
+                ReviewImage image = new ReviewImage(imageUrl, savedReview);
+                images.add(image);
+            }
+
+            reviewImageRepository.saveAll(images);
+        }
+
+        ReviewResDto resDto = new ReviewResDto(
+                savedReview.getId(),
+                restaurantId,
+                "Review and rating added successfully"
+        );
+        return resDto;
     }
 
     // 식당 리뷰 목록 조회
@@ -65,10 +79,15 @@ public class ReviewService {
     }
 
     private ReviewInfoDto convertToInfoDto(Review review) {
+        List<String> images = review.getImages().stream()
+                .map(ReviewImage::getImageUrl)
+                .collect(Collectors.toList());
+
         return ReviewInfoDto.builder()
                 .id(review.getId())
                 .content(review.getContent())
                 .rating(review.getRating())
+                .images(images)
                 .createdAt(review.getCreatedAt())
                 .updatedAt(review.getUpdatedAt())
                 .restaurantId(review.getRestaurant().getId())
