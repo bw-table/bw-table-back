5c26ddc4535a778d06b36551a5ca9d9641078ff5
jina <jinahoi@naver.com>
2024-11-06 19:11:20 +0900

    refactor: 예약 상태 변경에 커스텀 예외 적용
    - 예약 상태를 변경하기 전 검증하는 로직을 보완하고 그에 맞는 커스텀 예외를 적용했습니다.

--- src/main/java/com/zero/bwtableback/reservation/entity/Reservation.java
+++ src/main/java/com/zero/bwtableback/reservation/entity/Reservation.java
@@ -1,6 +1,15 @@
 package com.zero.bwtableback.reservation.entity;
 
+import static com.zero.bwtableback.common.exception.ErrorCode.CUSTOMER_CANCEL_TOO_LATE;
+import static com.zero.bwtableback.common.exception.ErrorCode.INVALID_STATUS_CONFIRM;
+import static com.zero.bwtableback.common.exception.ErrorCode.INVALID_STATUS_CUSTOMER_CANCEL;
+import static com.zero.bwtableback.common.exception.ErrorCode.INVALID_STATUS_NO_SHOW;
+import static com.zero.bwtableback.common.exception.ErrorCode.INVALID_STATUS_OWNER_CANCEL;
+import static com.zero.bwtableback.common.exception.ErrorCode.INVALID_STATUS_VISITED;
+
 import com.zero.bwtableback.common.BaseEntity;
+import com.zero.bwtableback.common.exception.CustomException;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,8 +17,11 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -51,8 +63,51 @@ public class Reservation extends BaseEntity {
 
 //    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL)
 //    private List<Payment> payments;
-//
-//    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL)
-//    private List<Notification> notifications;
+
+    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL)
+    private List<Notification> notifications;
+
+
+    public void confirmReservation() {
+        if (this.reservationStatus == ReservationStatus.CONFIRMED ||
+                this.reservationStatus == ReservationStatus.CUSTOMER_CANCELED ||
+                this.reservationStatus == ReservationStatus.OWNER_CANCELED ||
+                this.reservationStatus == ReservationStatus.NO_SHOW ||
+                this.reservationStatus == ReservationStatus.VISITED) {
+            throw new CustomException(INVALID_STATUS_CONFIRM);
+        }
+        this.reservationStatus = ReservationStatus.CONFIRMED;
+    }
+
+    public void cancelByCustomer() {
+        if (this.reservationStatus != ReservationStatus.CONFIRMED) {
+            throw new CustomException(INVALID_STATUS_CUSTOMER_CANCEL);
+        }
+        if (ChronoUnit.DAYS.between(reservationDate, LocalDate.now()) > 3) {
+            throw new CustomException(CUSTOMER_CANCEL_TOO_LATE);
+        }
+        this.reservationStatus = ReservationStatus.CUSTOMER_CANCELED;
+    }
+
+    public void cancelByOwner() {
+        if (this.reservationStatus != ReservationStatus.CONFIRMED) {
+            throw new CustomException(INVALID_STATUS_OWNER_CANCEL);
+        }
+        this.reservationStatus = ReservationStatus.OWNER_CANCELED;
+    }
+
+    public void markAsNoShow() {
+        if (this.reservationStatus != ReservationStatus.CONFIRMED) {
+            throw new CustomException(INVALID_STATUS_NO_SHOW);
+        }
+        this.reservationStatus = ReservationStatus.NO_SHOW;
+    }
+
+    public void markAsVisited() {
+        if (this.reservationStatus != ReservationStatus.CONFIRMED) {
+            throw new CustomException(INVALID_STATUS_VISITED);
+        }
+        this.reservationStatus = ReservationStatus.VISITED;
+    }
 
 }
