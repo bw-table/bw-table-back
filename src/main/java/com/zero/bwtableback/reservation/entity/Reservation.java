b5dbfd24e60da43a0f9617fbe75cbd4722fcf7e4
jina <jinahoi@naver.com>
2024-11-06 20:53:10 +0900

    refactor: 예약 내역 관련 커스텀 예외 코드 추가 및 적용 (#41)
    - `common` 패키지의 `ErrorCode` 클래스에 예약 내역 관련 커스텀 코드를 추가했습니다.
    - `Reservation`의 예약 상태 변경 메서드에 검증 로직을 추가 후 커스텀 코드를 적용했습니다.
    - `ReservationService`에서 반복되는 조회 메서드를 추출 후 커스텀 코드를 적용했습니다.
    - `ReservationRequestDto` 생성자에서 입력 값 검증 로직에 맞는 커스텀 코드를 적용했습니다.

--- src/main/java/com/zero/bwtableback/reservation/entity/Reservation.java
+++ src/main/java/com/zero/bwtableback/reservation/entity/Reservation.java
@@ -9,6 +9,8 @@ import static com.zero.bwtableback.common.exception.ErrorCode.INVALID_STATUS_VIS
 
 import com.zero.bwtableback.common.BaseEntity;
 import com.zero.bwtableback.common.exception.CustomException;
+import com.zero.bwtableback.member.entity.Member;
+import com.zero.bwtableback.restaurant.entity.Restaurant;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -17,6 +19,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -38,13 +42,13 @@ public class Reservation extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @ManyToOne
-//    @JoinColumn(updatable = false, nullable = false)
-//    private Restaurant restaurant;
-//
-//    @ManyToOne
-//    @JoinColumn(updatable = false, nullable = false)
-//    private User user;
+    @ManyToOne
+    @JoinColumn(updatable = false, nullable = false)
+    private Restaurant restaurant;
+
+    @ManyToOne
+    @JoinColumn(updatable = false, nullable = false)
+    private Member member;
 
     @Column(nullable = false)
     private LocalDate reservationDate;
@@ -55,17 +59,18 @@ public class Reservation extends BaseEntity {
     @Column(nullable = false)
     private int numberOfPeople;
 
-    private String specialRequests;
+    private String specialRequest;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ReservationStatus reservationStatus;
 
+    // Payment 엔티티 미작성으로 주석 처리
 //    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL)
 //    private List<Payment> payments;
-
-    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL)
-    private List<Notification> notifications;
+//
+//    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL)
+//    private List<Notification> notifications;
 
 
     public void confirmReservation() {
---
+++ src/main/java/com/zero/bwtableback/reservation/service/ReservationService.java
@@ -0,0 +1,132 @@
+package com.zero.bwtableback.reservation.service;
+
+import static com.zero.bwtableback.common.exception.ErrorCode.MEMBER_NOT_FOUND;
+import static com.zero.bwtableback.common.exception.ErrorCode.RESERVATION_FULL;
+import static com.zero.bwtableback.common.exception.ErrorCode.RESERVATION_NOT_FOUND;
+import static com.zero.bwtableback.common.exception.ErrorCode.RESTAURANT_NOT_FOUND;
+
+import com.zero.bwtableback.common.exception.CustomException;
+import com.zero.bwtableback.member.entity.Member;
+import com.zero.bwtableback.member.repository.MemberRepository;
+import com.zero.bwtableback.reservation.dto.ReservationRequestDto;
+import com.zero.bwtableback.reservation.dto.ReservationResponseDto;
+import com.zero.bwtableback.reservation.entity.Reservation;
+import com.zero.bwtableback.reservation.entity.ReservationStatus;
+import com.zero.bwtableback.reservation.repository.ReservationRepository;
+import com.zero.bwtableback.reservation.repository.ReservationSpecifications;
+import com.zero.bwtableback.restaurant.entity.Restaurant;
+import com.zero.bwtableback.restaurant.repository.RestaurantRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+    private final RestaurantRepository restaurantRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ReservationResponseDto> findReservationsWithFilters(Long restaurantId, Long memberId,
+                                                                    ReservationStatus reservationStatus,
+                                                                    LocalDate reservationDate,
+                                                                    LocalTime reservationTime, Pageable pageable) {
+
+        Specification<Reservation> spec = Specification.where(ReservationSpecifications.hasRestaurantId(restaurantId))
+                .and(ReservationSpecifications.hasMemberId(memberId))
+                .and(ReservationSpecifications.hasReservationStatus(reservationStatus))
+                .and(ReservationSpecifications.hasReservationDate(reservationDate))
+                .and(ReservationSpecifications.hasReservationTime(reservationTime));
+
+        Page<Reservation> reservationPage = reservationRepository.findAll(spec, pageable);
+
+        return reservationPage.map(ReservationResponseDto::fromEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public ReservationResponseDto getReservationById(Long reservationId) {
+        Reservation reservation = findReservationById(reservationId);
+        return ReservationResponseDto.fromEntity(reservation);
+    }
+
+    @Transactional
+    public ReservationResponseDto createReservation(ReservationRequestDto reservationRequestDto,
+                                                    Long restaurantId,
+                                                    Long memberId) {
+        validateReservationAvailability(restaurantId, reservationRequestDto);
+        Restaurant restaurant = findRestaurantById(restaurantId);
+        Member member = findMemberById(memberId);
+
+        Reservation reservation = ReservationRequestDto.toEntity(reservationRequestDto, restaurant, member);
+        Reservation savedReservation = reservationRepository.save(reservation);
+        return ReservationResponseDto.fromEntity(savedReservation);
+    }
+
+    @Transactional
+    public ReservationResponseDto confirmReservation(Long reservationId) {
+        Reservation reservation = findReservationById(reservationId);
+        reservation.confirmReservation();
+        return ReservationResponseDto.fromEntity(reservation);
+    }
+
+    @Transactional
+    public ReservationResponseDto cancelReservationByCustomer(Long reservationId) {
+        Reservation reservation = findReservationById(reservationId);
+        reservation.cancelByCustomer();
+        return ReservationResponseDto.fromEntity(reservation);
+    }
+
+    @Transactional
+    public ReservationResponseDto cancelReservationByOwner(Long reservationId) {
+        Reservation reservation = findReservationById(reservationId);
+        reservation.cancelByOwner();
+        return ReservationResponseDto.fromEntity(reservation);
+    }
+
+    @Transactional
+    public ReservationResponseDto markReservationAsNoShow(Long reservationId) {
+        Reservation reservation = findReservationById(reservationId);
+        reservation.markAsNoShow();
+        return ReservationResponseDto.fromEntity(reservation);
+    }
+
+    @Transactional
+    public ReservationResponseDto markReservationAsVisited(Long reservationId) {
+        Reservation reservation = findReservationById(reservationId);
+        reservation.markAsVisited();
+        return ReservationResponseDto.fromEntity(reservation);
+    }
+
+    private Reservation findReservationById(Long reservationId) {
+        return reservationRepository.findById(reservationId)
+                .orElseThrow(() -> new CustomException(RESERVATION_NOT_FOUND));
+    }
+
+    private Restaurant findRestaurantById(Long restaurantId) {
+        return restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new CustomException(RESTAURANT_NOT_FOUND));
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+    }
+
+    private void validateReservationAvailability(Long restaurantId, ReservationRequestDto reservationRequestDto) {
+        reservationRepository.findByRestaurantIdAndReservationDateAndReservationTime(
+                        restaurantId,
+                        reservationRequestDto.reservationDate(),
+                        reservationRequestDto.reservationTime())
+                .ifPresent(r -> {
+                    throw new CustomException(RESERVATION_FULL);
+                });
+    }
+
+}
