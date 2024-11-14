ba4002fdea09aba182a1064bb83f84baa3d724ce
jina <jinahoi@naver.com>
2024-11-15 00:10:49 +0900

    refactor: 예약 서비스 에러 코드 추가
    - 예약 상태 업데이트 메서드에서 사용하는 에러 코드가 추가되었습니다.

--- src/main/java/com/zero/bwtableback/reservation/service/ReservationService.java
+++ src/main/java/com/zero/bwtableback/reservation/service/ReservationService.java
@@ -80,7 +80,7 @@ public class ReservationService {
         }
 
         if (statusUpdateDto.reservationStatus() == null) {
-//          FIXME  throw new CustomException(ErrorCode.INVALID_RESERVATION_STATUS);
+          throw new CustomException(ErrorCode.INVALID_RESERVATION_STATUS);
         }
 
         ReservationStatus newStatus = statusUpdateDto.reservationStatus();
@@ -90,8 +90,7 @@ public class ReservationService {
             case OWNER_CANCELED -> handleOwnerCanceledStatus(reservation);
             case NO_SHOW -> handleNoShowStatus(reservation);
             case VISITED -> handleVisitedStatus(reservation);
-//          FIXME  default -> throw new CustomException(ErrorCode.INVALID_RESERVATION_STATUS);
-            default -> throw new RuntimeException("INVALID_RESERVATION_STATUS");
+            default -> throw new CustomException(ErrorCode.INVALID_RESERVATION_STATUS);
         };
     }
 
