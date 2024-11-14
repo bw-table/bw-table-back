8d205307558fed2755456182e2dfc9780412be79
jina <jinahoi@naver.com>
2024-11-15 02:30:22 +0900

    feat: 알림 수신을 위한 엔드포인트 추가
    - 클라이언트가 SSE 연결로 실시간 데이터를 수신하는 `/subscribe`엔드포인트가 추가했습니다.
    - 사용자 인증 정보를 받아와 해당 사용자에 대한 알림을 전송합니다.
    - `Last-Event-ID` 헤더를 추가해 클라이언트가 해당 값을 전달할 경우 알림 누락을 방지합니다.
    - 알림 목록을 회원이 직접 조회할 경우 데이터를 반환할 dto 클래스를 추가했습니다.

--- src/main/java/com/zero/bwtableback/reservation/controller/NotificationController.java
+++ src/main/java/com/zero/bwtableback/reservation/controller/NotificationController.java
@@ -1,21 +1,27 @@
 package com.zero.bwtableback.reservation.controller;
 
-//import com.zero.bwtableback.reservation.dto.NotificationResDto;
+import com.zero.bwtableback.reservation.dto.NotificationResDto;
 import com.zero.bwtableback.reservation.entity.Notification;
 import com.zero.bwtableback.reservation.entity.NotificationType;
 import com.zero.bwtableback.reservation.entity.Reservation;
+import com.zero.bwtableback.reservation.service.NotificationEmitterService;
 import com.zero.bwtableback.reservation.service.NotificationSearchService;
 import com.zero.bwtableback.reservation.service.ReservationService;
+import com.zero.bwtableback.security.MemberDetails;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Slf4j
 @RestController
@@ -24,22 +30,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
 
     private final ReservationService reservationService;
+    private final NotificationEmitterService notificationEmitterService;
     private final NotificationSearchService notificationSearchService;
+
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@AuthenticationPrincipal MemberDetails memberDetails,
+                                @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "")
+                                String lastEventId) {
+        return notificationEmitterService.subscribe(memberDetails.getMemberId(), lastEventId);
+    }
 
     // 고객 회원에게 전송된 알림 목록 조회
     @GetMapping("/customers/{userId}")
     public Page<?> getCustomerNotifications(@PathVariable Long userId, Pageable pageable) {
         Page<Notification> notifications = notificationSearchService.getNotificationsSentToCustomer(userId, pageable);
-//        return notifications.map(NotificationResDto::fromEntity);
-        return null;
+        return notifications.map(NotificationResDto::fromEntity);
     }
 
     // 가게 주인 회원에게 전송된 알림 목록 조회
     @GetMapping("/owners/{ownerId}")
     public Page<?> getOwnerNotifications(@PathVariable Long ownerId, Pageable pageable) {
         Page<Notification> notifications = notificationSearchService.getNotificationsSentToOwner(ownerId, pageable);
-//        return notifications.map(NotificationResDto::fromEntity);
-        return null;
+        return notifications.map(NotificationResDto::fromEntity);
     }
 
     // 메시지 생성에 사용할 수 있는 알림 정보 반환
