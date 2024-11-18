9ac1f40b4464acbcb601f1291617e3b64d9a8ca2
jina <jinahoi@naver.com>
2024-11-18 23:01:48 +0900

    refactor: 스케쥴러 예외 처리 로직 간소화
    - 내부에서 이미 예외 로그를 남기는 방법으로 예외 처리를 하고 있어 불필요한 예외 처리를 생략합니다.

--- src/main/java/com/zero/bwtableback/reservation/scheduler/NotificationJobScheduler.java
+++ src/main/java/com/zero/bwtableback/reservation/scheduler/NotificationJobScheduler.java
@@ -19,24 +19,22 @@ public class NotificationJobScheduler {
     private final Job deleteOldNotificationsJob;
 
     @Scheduled(cron = "0 0 8 * * ?") // 매일 아침 8시에 실행
-    public void scheduleSendDayOfVisitNotifications() throws JobExecutionException {
+    public void scheduleSendDayOfVisitNotifications() {
         try {
             jobLauncher.run(sendDayOfVisitNotificationJob, new JobParameters());
             log.info("당일 예약 알림 전송을 성공적으로 완료했습니다.");
         } catch (Exception e) {
-            log.error("당일 예약 알림 전송이 실패했습니다", e);
-            throw new JobExecutionException("당일 예약 알림 전송이 실패했습니다.", e);
+            log.error("당일 예약 알림 전송이 실패했습니다.", e);
         }
     }
 
     @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
-    public void scheduleDeleteOldNotifications() throws JobExecutionException {
+    public void scheduleDeleteOldNotifications() {
         try {
             jobLauncher.run(deleteOldNotificationsJob, new JobParameters());
             log.info("일주일 지난 알림 삭제를 성공적으로 완료했습니다.");
         } catch (Exception e) {
             log.error("일주일 지난 알림 삭제가 실패했습니다.", e);
-            throw new JobExecutionException("일주일 지난 알림 삭제가 실패했습니다.", e);
         }
     }
 
