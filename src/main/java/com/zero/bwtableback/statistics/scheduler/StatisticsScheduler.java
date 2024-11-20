f5bfdd22c2c4ba68e3a61aa43b2f3f7ab7074e5f
jina <jinahoi@naver.com>
2024-11-21 03:25:13 +0900

    feat: 통계 배치 스케쥴링 추가 (#74)
    - 각 통계 유형에 맞게 스케쥴링 설정을 분리했습니다.
    - 일별 건수는 매일 집계 매일 새벽 1시에 스케쥴링되었습니다.
    - 주별 건수는 조회 쿼리가 일요일부터 토요일을 한 주로 정의하기 때문에 일요일로 스케쥴링되었습니다.
    - 월별 건수는 매월 1일 업데이트 되도록 스케쥴링되었습니다.

---
+++ src/main/java/com/zero/bwtableback/statistics/scheduler/StatisticsScheduler.java
@@ -0,0 +1,73 @@
+package com.zero.bwtableback.statistics.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StatisticsScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final Job dailyStatisticsJob;
+    private final Job weeklyStatisticsJob;
+    private final Job monthlyStatisticsJob;
+    private final Job popularTimeSlotsJob;
+    private final Job popularDatesJob;
+
+    @Scheduled(cron = "0 0 1 * * ?") // 매일 새벽 1시 실행
+    public void runDailyStatisticsJob() throws Exception {
+        try {
+            jobLauncher.run(dailyStatisticsJob, new JobParameters());
+            log.info("일별 예약 통계 저장을 성공적으로 완료했습니다.");
+        } catch (Exception e) {
+            log.error("일별 예약 통계 저장에 실패했습니다.", e);
+        }
+    }
+
+    @Scheduled(cron = "0 0 2 ? * SUN") // 매주 일요일 새벽 2시 실행
+    public void runWeeklyStatisticsJob() throws Exception {
+        try {
+            jobLauncher.run(weeklyStatisticsJob, new JobParameters());
+            log.info("주별 예약 통계 저장을 성공적으로 완료했습니다.");
+        } catch (Exception e) {
+            log.error("주별 예약 통계 저장에 실패했습니다.", e);
+        }
+    }
+
+    @Scheduled(cron = "0 0 3 1 * ?") // 매월 1일 새벽 3시 실행
+    public void runMonthlyStatisticsJob() {
+        try {
+            jobLauncher.run(monthlyStatisticsJob, new JobParameters());
+            log.info("월별 예약 통계 저장을 성공적으로 완료했습니다.");
+        } catch (Exception e) {
+            log.error("월별 예약 통계 저장에 실패했습니다.", e);
+        }
+    }
+
+    @Scheduled(cron = "0 0 4 * * ?") // 매일 새벽 4시 실행
+    public void runPopularTimeSlotsJob() {
+        try {
+            jobLauncher.run(popularTimeSlotsJob, new JobParameters());
+            log.info("인기 예약 시간대 통계 저장을 성공적으로 완료했습니다.");
+        } catch (Exception e) {
+            log.error("인기 예약 시간대 통계 저장에 실패했습니다.", e);
+        }
+    }
+
+    @Scheduled(cron = "0 0 5 * * ?") // 매일 새벽 5시 실행
+    public void runPopularDatesJob() {
+        try {
+            jobLauncher.run(popularDatesJob, new JobParameters());
+            log.info("인기 예약 일자 통계 저장을 성공적으로 완료했습니다.");
+        } catch (Exception e) {
+            log.error("인기 예약 일자 통계 저장에 실패했습니다.", e);
+        }
+    }
+
+}
